66bf8cf490a8e4b99b1fd5614327737887146981
xinghunm <xinghunm@keyayun.com>
2023-04-11 17:19:46 +0800

    update chinese chars

--- src/characters.js
+++ src/characters.js
@@ -1,6 +1,6 @@
-const chineseChars = require('./chinese-chars');
+const chineseChars = require('./chinese-chars-full');
 
 const BASE_CHARS = `!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_\abcdefghijklmnopqrstuvwxyz{|}~¥®·—…、。《》「」【】〔〕〜`;
 const PUNCTUATION = `（），｜≤≥！：；？‘’“”`;
-
+//字符 ≤ 和 ≥ 无法显，通过glyph手动添加
 module.exports = `/.notdef\u0001` + BASE_CHARS + PUNCTUATION + chineseChars;
